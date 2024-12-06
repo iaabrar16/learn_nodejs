1a803898e88ad99aed7af41aab1c24db514e9042
Ismail Aabrar <kingaabrar@gmail.com>
2024-12-06 20:02:51 +0600

    Module - exports, require done

---
+++ student.js
@@ -0,0 +1,19 @@
+const getName =()=>{
+    return "Aabrar"
+}
+
+const getAge =()=>{
+    return "25"
+}
+
+const cgpa=3.00;
+
+// exports.Name = getName;
+// exports.Age = getAge;
+// exports.Result = cgpa;
+
+module.exports ={
+    getAge,
+    getName,
+    cgpa
+}
