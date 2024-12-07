e97d565524659925b0dec03a6d32add1f69f050e
Ismail Aabrar <kingaabrar@gmail.com>
2024-12-08 05:04:18 +0600

    server create done

--- L-4/server.js
+++ L-4/server.js
@@ -1,13 +1,14 @@
 const http = require('http');
-const hostname = '127.0.0.1';
+
 const port = 3000;
+const hostname = '127.0.0.1';
 
 
 const myServer = http.createServer((req,res)=>{
     res.end("<h1>First Server</h1>");
 });
 
-myServer.listen(hostname,port, ()=>{
+myServer.listen(port, hostname, ()=>{
     console.log(`Server is running successfully at http://${hostname}:${port}`);
 });
 
